dd878bd0bd805bbe3b754b88c730217d1309b920
alexcesarsilva <alexcesars@icloud.com>
2021-04-20 19:38:23 -0300

    better reverse.py

--- Learning/reverse.py
+++ Learning/reverse.py
@@ -2,7 +2,5 @@ number = int(input("Enter your number"))
 while int(number) != 0:
     reverse = int(number) % 10
     print(int(reverse), end="")
-    if reverse == 0:
-        break
     number /= 10
 print()
