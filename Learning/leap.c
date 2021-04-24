140927bd4c1ba92eb2ae345ec7ccfc9a488c57ba
alexcesarsilva <alexcesars@icloud.com>
2021-04-23 21:08:30 -0300

    leap year or not lleap year

---
+++ Learning/leap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * @param remainder rem 
+ * @return leap or not leap
+  */
+int main(void)
+{
+    int year, rem4, rem100, rem400;
+    printf("prompt: ");
+    scanf("%i", &year);
+
+    rem4 = year % 4;
+    rem100 = year % 100;
+    rem400 = year % 400;
+
+    if ((rem4 == 0 && rem100 != 0) || rem400 == 0) {
+        puts("It's a leap year.");
+    }
+    else {
+        puts("Nope, is not a leap year.");
+    }
+}
