986dab04def610945fb1bdcc95f5c1501530996f
alexcesarsilva <alexcesars@icloud.com>
2021-05-01 19:18:11 -0300

    add square pow and root

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -3,12 +3,16 @@ class Calculator {
      * Enter in command-line run.
      * @param args Asks number, operator, another number.
      * @return result.
+     * @author alexcesarsilva
      */
+    public static void println(String s) {
+        System.out.println(s);
+    }
     public static void main(String[] args) {
-        float x = Float.parseFloat(args[0]);
-        float y = Float.parseFloat(args[2]);
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[2]);
 
-        // if(args[0].equals("sum")) or switch:
+        // Using switch or if(args[1].equals("sum")).
         switch(args[1]) {
             case "add" : sum(x, y);
                 break;
@@ -16,30 +20,47 @@ class Calculator {
                 break;
             case "times" : mult(x, y);
                 break;
-            case "divided" : div(x, y);
+            case "div" : 
+                if (args[2].equals(0)) {
+                    println("Division by zero is not available!");
+                }
+                else { 
+                    div(x, y);
+                }
+                break;
+            case "pow" : power(x, y);
+                break;
+            case "root" : root(x, y);
                 break;
             default :  error();
             break;
         }
     }
 
-    static void sum(float x, float y) {
+    static void sum(double x, double y) {
         System.out.println(x + y);
     }
 
-    static void diff(float x, float y) {
+    static void diff(double x, double y) {
         System.out.println(x - y);
     }
 
-    static void mult(float x, float y) {
+    static void mult(double x, double y) {
         System.out.println(x * y);
     }
 
-    static void div(float x, float y) {
-        System.out.printf("%.2f\n", x / y);
+    static void div(double x, double y) {
+        System.out.printf("\n\t%.2f\n", x / y);
+    }
+
+    static void power(double x, double y) {
+        System.out.printf("\n\t%g\n", Math.pow(x, y));
+    }
+    static void root(double x, double y) {
+        System.out.printf("\n\t%.0f\n", Math.sqrt(y));
     }
 
     static void error() {
-        System.out.println("Oops! Invalid operator.");
+        System.out.println("\n\tOops! Invalid operator.");
     }
 }
